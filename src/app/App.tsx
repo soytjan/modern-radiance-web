9848b74127116b533ed29169c6b8c395a8a26d96
Amanda <amanda@molecule.io>
2022-01-16 18:29:28 +0100

    Modify and add variables.scss

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,19 +4,7 @@ import './App.scss';
 export const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
