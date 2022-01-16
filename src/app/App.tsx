53e4ab03ffaa793152522f6a4ad565595f6a2ebe
Amanda <amanda@molecule.io>
2022-01-16 17:55:01 +0100

    Create app dir and add sass

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -22,5 +20,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
